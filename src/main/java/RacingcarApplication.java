0d6ca4ea8a567ba24cda739938e266a60f2170de
hjjangDev <ju0398@hanmail.net>
2021-05-03 15:49:59 +0900

    feat: 자동차 경주 게임 시작 호출 class 추가

---
+++ src/main/java/RacingcarApplication.java
@@ -0,0 +1,10 @@
+import racingcar.domin.Game;
+
+public class RacingcarApplication {
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.start();
+
+    }
+}
